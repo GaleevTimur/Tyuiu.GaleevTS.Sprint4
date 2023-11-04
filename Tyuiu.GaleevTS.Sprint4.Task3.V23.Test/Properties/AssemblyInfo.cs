e8ad21dab7107f4ee1e8118380a4c7ffa6db5590
GaleevTimur <timur.galeev.05@yandex.ru>
2023-11-04 22:23:52 +0500

    Task3

---
+++ Tyuiu.GaleevTS.Sprint4.Task3.V23.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.GaleevTS.Sprint4.Task3.V23.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.GaleevTS.Sprint4.Task3.V23.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("062edcd0-e462-44bf-8905-8aa4c857266a")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
--- Tyuiu.GaleevTS.Sprint4.Task3.V23/Program.cs
+++ Tyuiu.GaleevTS.Sprint4.Task3.V23/Program.cs
@@ -3,22 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Tyuiu.GaleevTS.Sprint4.Task2.V26.Lib;
+using Tyuiu.GaleevTS.Sprint4.Task3.V23.Lib;
 
-namespace Tyuiu.GaleevTS.Sprint4.Task2.V26
+namespace Tyuiu.GaleevTS.Sprint4.Task3.V23
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Random rnd = new Random();
             DataService ds = new DataService();
             Console.Title = "Спринт №4 | Выполнил: Галеев Т. С. | ИИПб-23-3";
             Console.WriteLine("****************************************************************************");
             Console.WriteLine("* Спринт #4                                                                *");
-            Console.WriteLine("* Тема: Одномерные массивы. (генератор случайных чисел)                    *");
-            Console.WriteLine("* Задание №2                                                               *");
-            Console.WriteLine("* Вариант #26                                                              *");
+            Console.WriteLine("* Тема: Двумерные массивы. (статический ввод)                              *");
+            Console.WriteLine("* Задание №3                                                               *");
+            Console.WriteLine("* Вариант #23                                                              *");
             Console.WriteLine("* Выполнил: Галеев Тимур Серикович | ИИПб-23-3                             *");
             Console.WriteLine("****************************************************************************");
             Console.WriteLine("*                                                                          *");
@@ -26,30 +25,29 @@ namespace Tyuiu.GaleevTS.Sprint4.Task2.V26
             Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ                                                          *");
             Console.WriteLine("****************************************************************************");
 
-            int len;
-            int multArray;
-            Console.Write("Введите количество элементов массива: ");
-            len = Convert.ToInt32(Console.ReadLine());
+            int[,] array = new int[5,5] { { 7, 5, 5, 6, 7 },
+                                          { 8, 7, 8, 4, 3 },
+                                          { 5, 6, 7, 8, 3 },
+                                          { 4, 2, 3, 6, 4 },
+                                          { 5, 2, 4, 2, 3 } };
 
-            int[] array = new int[len];
-            for (int i = 0; i <= len - 1; i++)
-            {
-                array[i] = rnd.Next(1, 9);
-            }
-
-            Console.WriteLine();
+            int rows = array.GetUpperBound(0) + 1;
+            int columns = array.Length / rows;
 
             Console.WriteLine("Массив: ");
-            for (int i = 0; i <= len - 1; i++)
+            for (int i = 0; i < rows; i++)
             {
-                Console.Write(array[i] + "\t");
+                for (int j = 0; j < columns; j++)
+                {
+                    Console.Write($"{array[i,j]} \t");
+                }
+                Console.WriteLine();
             }
-            Console.WriteLine();
-            multArray = ds.Calculate(array);
+            int count = ds.Calculate(array);
             Console.WriteLine("****************************************************************************");
             Console.WriteLine("* РЕЗУЛЬТАТ:                                                               *");
             Console.WriteLine("****************************************************************************");
-            Console.WriteLine("Произведение нечётных элементов массива = " + multArray);
+            Console.WriteLine("Количество чётных элементов в массиве = " + count);
 
 
             Console.ReadKey();
